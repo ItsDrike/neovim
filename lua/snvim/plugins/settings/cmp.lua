8543626b52f83b472f312cc228530a80a640f7cf
ItsDrike <itsdrike@protonmail.com>
2021-12-13 11:19:31 +0100

    Enable path, buffer and cmdline cmp sources

--- lua/snvim/plugins/settings/cmp.lua
+++ lua/snvim/plugins/settings/cmp.lua
@@ -3,5 +3,10 @@ local cmp = require("cmp")
 cmp.setup({
     sources = {
         { name = "nvim_lsp" },
-    }
+        { name = "path" },
+        { name = "buffer" },
+    },
 })
+
+cmp.setup.cmdline('/', { sources = { name = 'buffer' } })
+cmp.setup.cmdline(':', { sources = { name = 'cmdine' } })
