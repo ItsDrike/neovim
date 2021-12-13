4f8d43bf680479109b5b57a39dd2c505f5afab61
ItsDrike <itsdrike@protonmail.com>
2021-12-13 11:20:36 +0100

    Limit cmp completions width to 50 chars

--- lua/snvim/plugins/settings/cmp.lua
+++ lua/snvim/plugins/settings/cmp.lua
@@ -6,6 +6,17 @@ cmp.setup({
         { name = "path" },
         { name = "buffer" },
     },
+    formatting = {
+        fields = { "kind", "abbr", "menu" },
+        format = function(entry, vim_item)
+            -- Limit completion hint width
+            local maxwidth = 50
+            if #vim_item.abbr > maxwidth then
+                vim_item.abbr = vim_item.abbr:sub(1, maxwidth) .. "..."
+            end
+            return vim_item
+        end,
+    },
     mapping = {
         ['<Tab>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
         ['<S-Tab>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
