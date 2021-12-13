f1d292adade1d9296077757af2ad6ab8ce904377
ItsDrike <itsdrike@protonmail.com>
2021-12-13 11:19:46 +0100

    Add cmp mappings

--- lua/snvim/plugins/settings/cmp.lua
+++ lua/snvim/plugins/settings/cmp.lua
@@ -6,6 +6,18 @@ cmp.setup({
         { name = "path" },
         { name = "buffer" },
     },
+    mapping = {
+        ['<Tab>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+        ['<S-Tab>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+        ['<C-n>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+        ['<C-p>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+        ['<Down>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+        ['<Up>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
+        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<CR>'] = cmp.mapping.confirm { behavior = cmp.ConfirmBehavior.Replace, select = true },
+  },
 })
 
 cmp.setup.cmdline('/', { sources = { name = 'buffer' } })
