9a2a81c5e529f68466a6b438105e719b5eb87ee0
ItsDrike <itsdrike@protonmail.com>
2021-12-14 15:51:06 +0100

    Use lua for telescope keymaps

--- lua/snvim/plugins/settings/telescope.lua
+++ lua/snvim/plugins/settings/telescope.lua
@@ -1,9 +1,11 @@
 local m = require("snvim.utility.mappings")
 
-m.keymap("n", "ff", "<cmd>Telescope find_files<CR>")
-m.keymap("n", "fg", "<cmd>Telescope live_grep<CR>")
-m.keymap("n", "fb", "<cmd>Telescope buffers<CR>")
-m.keymap("n", "fh", "<cmd>Telescope help_tags<CR>")
-m.keymap("n", "fgb", "<cmd>Telescope git_branches<CR>")
-m.keymap("n", "fgc", "<cmd>Telescope git_commits<CR>")
-m.keymap("n", "fgs", "<cmd>Telescope git_status<CR>")
+local prefix = "<cmd>lua require('telescope.builtin')."
+
+m.keymap("n", "ff", prefix .. "find_files()<CR>")
+m.keymap("n", "fg", prefix .. "live_grep()<CR>")
+m.keymap("n", "fb", prefix .. "buffers()<CR>")
+m.keymap("n", "fh", prefix .. "help_tags()<CR>")
+m.keymap("n", "fgb", prefix .. "git_branches()<CR>")
+m.keymap("n", "fgc", prefix .. "git_commits()<CR>")
+m.keymap("n", "fgs", prefix .. "git_status()<CR>")
