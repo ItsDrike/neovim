75e3a4878991bf19931bfbff8c90a55044b69ab1
ItsDrike <itsdrike@protonmail.com>
2021-12-14 15:33:43 +0100

    Update LSP keymaps

--- lua/snvim/lsp/keymaps.lua
+++ lua/snvim/lsp/keymaps.lua
@@ -7,53 +7,60 @@ local m = require("snvim.utility.mappings")
 local telescope_installed, _ = pcall(require, "telescope")
 local lsp_signature_installed, _ = pcall(require, "lsp_signature")
 
+-- We usually always use the same prefixes, so just define them here
+local lua_prefix = "<cmd>lua "
+local lsp_prefix = lua_prefix .. "vim.lsp.buf."
+local diagnostic_prefix = lua_prefix .. "vim.diagnostic."
+local telescope_prefix = lua_prefix .. "require('telescope.builtin')."
+local telescope_cursor_theme = "require('telescope.themes').get_cursor()"
+
 -- Lookups
-m.keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>")
-m.keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>')
-m.keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>")
-m.keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>")
-m.keymap("n", "gt", "<cmd>lua vim.lsp.buf.type_definition()<CR>")
+m.keymap("n", "gd", lsp_prefix .. "definition()<CR>")
+m.keymap('n', 'gD', lsp_prefix .. 'declaration()<cr>')
+m.keymap("n", "gr", lsp_prefix .. "references()<CR>")
+m.keymap("n", "gi", lsp_prefix .. "implementation()<CR>")
+m.keymap("n", "gt", lsp_prefix .. "type_definition()<CR>")
 
 if telescope_installed then
-    m.keymap('n', 'gd', '<cmd>lua require("telescope.builtin").lsp_definitions()<cr>')
-    m.keymap('n', 'gr', '<cmd>lua require("telescope.builtin").lsp_references()<cr>')
-    m.keymap('n', 'gi', '<cmd>lua require("telescope.builtin").lsp_implementations()<cr>')
-    m.keymap('n', 'gt', '<cmd>lua require("telescope.builtin").lsp_type_definitions()<cr>')
+    m.keymap('n', 'gd', telescope_prefix .. 'lsp_definitions()<cr>')
+    m.keymap('n', 'gr', telescope_prefix .. 'lsp_references()<cr>')
+    m.keymap('n', 'gi', telescope_prefix .. 'lsp_implementations()<cr>')
+    m.keymap('n', 'gt', telescope_prefix .. 'lsp_type_definitions()<cr>')
 end
 
 -- Formatting
-m.keymap('n', '<leader>gf', '<cmd>lua vim.lsp.buf.formatting()<cr>')
-m.keymap('v', '<leader>gf', '<cmd>lua vim.lsp.buf.range_formatting()<cr>')
+m.keymap('n', '<leader>gf', lsp_prefix .. 'formatting()<cr>')
+m.keymap('v', '<leader>gf', lsp_prefix .. 'range_formatting()<cr>')
 
 -- Hover info
-m.keymap("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>")
-m.keymap("n", "M", "<cmd>lua vim.lsp.buf.hover()<CR>")
+m.keymap("n", "<C-k>", lsp_prefix .. "signature_help()<CR>")
+m.keymap("n", "M", lsp_prefix .. "hover()<CR>")
 
 if lsp_signature_installed then
-    m.keymap('n', '<C-M>', '<cmd>lua require("lsp_signature").signature()<cr>')
+    m.keymap('n', '<C-M>', lua_prefix .. 'require("lsp_signature").signature()<cr>')
 end
 
 -- Diagnostics
-m.keymap('n', '[g', '<cmd>lua vim.diagnostic.goto_prev()<cr>')
-m.keymap('n', ']g', '<cmd>lua vim.diagnostic.goto_next()<cr>')
-m.keymap('n', 'ge', '<cmd>lua vim.diagnostic.open_float(nil, { scope = "line", })<cr>')
+m.keymap('n', '[g', diagnostic_prefix .. 'goto_prev()<cr>')
+m.keymap('n', ']g', diagnostic_prefix .. 'goto_next()<cr>')
+m.keymap('n', 'ge', diagnostic_prefix .. 'open_float(nil, { scope = "line", })<cr>')
 
 if telescope_installed then
-    m.keymap('n', '<leader>ge', '<cmd>lua require("telescope.builtin").lsp_document_diagnostics()<cr>')
+    m.keymap('n', '<leader>ge', telescope_prefix .. 'diagnostics()<cr>')
 end
 
 -- LSP Workspace
-m.keymap('n', '<leader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<cr>')
-m.keymap('n', '<leader>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<cr>')
-m.keymap('n', '<leader>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<cr>')
+m.keymap('n', '<leader>wa', lsp_prefix .. 'add_workspace_folder()<cr>')
+m.keymap('n', '<leader>wr', lsp_prefix .. 'remove_workspace_folder()<cr>')
+m.keymap('n', '<leader>wl', lua_prefix .. 'print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<cr>')
 
 --Actions
-m.keymap('n', '<leader>ga', '<cmd>lua vim.lsp.buf.code_action()<cr>')
-m.keymap('v', '<leader>ga', '<cmd>lua require("telescope.builtin").range_code_action()<cr>')
+m.keymap('n', '<leader>ga', lsp_prefix .. 'code_action()<cr>')
+m.keymap('v', '<leader>ga', lsp_prefix .. 'range_code_action()<cr>')
 if telescope_installed then
-    m.keymap('n', '<leader>ga', '<cmd>lua require("telescope.builtin").lsp_code_actions()<cr>')
-    m.keymap('v', '<leader>ga', '<cmd>lua require("telescope.builtin").lsp_range_code_actions()<cr>')
+    m.keymap('n', '<leader>ga', telescope_prefix .. 'lsp_code_actions(' .. telescope_cursor_theme .. ')<cr>')
+    m.keymap('v', '<leader>ga', telescope_prefix .. 'lsp_range_code_actions(' .. telescope_cursor_theme .. ')<cr>')
 end
 
 -- Use custom implementation for renaming all references
-m.keymap('n', 'gn', '<cmd>lua require("snvim.lsp.rename").rename()<cr>')
+m.keymap('n', 'gn', lua_prefix .. 'require("snvim.lsp.rename").rename()<cr>')
