157d3fa22bfde9624ee98f848ad29b17218a9538
ItsDrike <itsdrike@protonmail.com>
2021-12-20 14:17:26 +0100

    Fix comments

--- lua/snvim/lsp/utils.lua
+++ lua/snvim/lsp/utils.lua
@@ -12,7 +12,8 @@ function M.is_server_active(server_name)
 end
 
 -- Check if the manager autocmd has already been configured since some servers can take a while to initialize
--- this helps guarding against a data-race condition where a server can get configured twice which seems to occur only when attaching to single-files
+-- this helps guarding against a data-race condition where a server can get configured twice which seems
+-- to occur only when attaching to single-files
 function M.is_server_configured(server_name, filetype)
     filetype = filetype or vim.bo.filetype
     local active_autocmds = vim.split(vim.fn.execute("autocmd FileType" .. filetype), "\n")
@@ -25,7 +26,7 @@ function M.is_server_configured(server_name, filetype)
 end
 
 -- Get a list of supported filetypes from a given language server (by name)
--- This required nvim-lsp-installer plugin
+-- This requires nvim-lsp-installer plugin
 function M.get_supported_filetypes(server_name)
     local lsp_installer_servers = require("nvim-lsp-installer.servers")
     local server_available, requested_server = lsp_installer_servers.get_server(server_name)
