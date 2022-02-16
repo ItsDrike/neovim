de82655c80dba80357934b93f29b2600afc8b41a
ItsDrike <itsdrike@protonmail.com>
2022-02-16 11:15:42 +0100

    Add write_file utility function

--- lua/snvim/utils/path.lua
+++ lua/snvim/utils/path.lua
@@ -1,3 +1,5 @@
+local uv = vim.loop
+
 local M = {}
 
 local os_name = vim.loop.os_uname().version
@@ -41,6 +43,24 @@ end
 function M.is_file(path)
     local stat = vim.loop.fs_stat(path)
     return stat and stat.type == "file" or false
+end
+
+
+---Write data to a file
+---@param path string can be full or relative to `cwd`
+---@param txt string|table text to be written, uses `vim.inspect` internally for tables
+---@param flag string used to determine access mode, common flags: "w" for `overwrite` or "a" for `append`
+function M.write_file(path, txt, flag)
+  local data = type(txt) == "string" and txt or vim.inspect(txt)
+  uv.fs_open(path, flag, 438, function(open_err, fd)
+    assert(not open_err, open_err)
+    uv.fs_write(fd, data, -1, function(write_err)
+      assert(not write_err, write_err)
+      uv.fs_close(fd, function(close_err)
+        assert(not close_err, close_err)
+      end)
+    end)
+  end)
 end
 
 
