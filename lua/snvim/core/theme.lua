a581a0868da26bd3f0f6925907411f321c27b51b
ItsDrike <itsdrike@protonmail.com>
2022-03-23 17:26:32 +0100

    Add more theme-related settings

--- lua/snvim/core/theme.lua
+++ lua/snvim/core/theme.lua
@@ -1,8 +1,38 @@
+local utils = require "snvim.utils.core"
 local settings = require "snvim.utils.settings"
-
--- TODO: Change this to autocmd running on ColorScheme
--- after we set up a hook running after packer init
 
 local config = settings.get_settings("theme")
 
+vim.opt.cursorline = config.show_cursor_line
+vim.opt.number = config.show_line_numbers
+vim.opt.relativenumber = config.show_relative_numbers
+vim.opt.showmatch = config.show_matching_brackets
+vim.opt.scrolloff = config.horizontal_scrolloff
+vim.opt.sidescrolloff = config.vertical_scrolloff
+vim.opt.wrap = config.enable_line_wrapping
+
+-- Make sure to utilize the full 256 colors of most modern terminals,
+-- if we have $DISPLAY env variable set (we're in a graphical session, not pure TTY)
+vim.opt.termguicolors = os.getenv("DISPLAY") and true or false
+
+utils.define_augroup(
+    "_cursor_color",
+    {
+        {
+            "ColorScheme",
+            "*",
+            "highlight CursorLine guibg=" .. config.cursor_line_guibg ..
+                " ctermbg=" .. config.cursor_line_ctermbg
+        },
+        {
+            "ColorScheme",
+            "*",
+            "highlight CursorLineNr guifg=" .. config.cursor_line_number_guifg ..
+                " ctermfg=" .. config.cursor_line_number_ctermfg
+        }
+    }
+)
+
+-- Set the colorscheme last, since it also triggers the ColorScheme autocmds,
+-- which are defined above
 vim.cmd("colorscheme " .. config.colorscheme)
--- lua/snvim/settings/theme.lua
+++ lua/snvim/settings/theme.lua
@@ -1,7 +1,20 @@
 local M = {}
 
 M.config = {
-    colorscheme = "codedark"
+    colorscheme = "codedark",
+    show_cursor_line=true,
+    show_line_numbers=true,
+    show_relative_numbers=true,
+    show_matching_brackets=true,
+    enable_line_wrapping=true,
+    vertical_scrolloff=5,
+    horizontal_scrolloff=5,
+
+    -- Make cursor line more noticable
+    cursor_line_guibg = "#2b2b2b",
+    cursor_line_ctermbg = "None",
+    cursor_line_number_guifg = "#1f85de",
+    cursor_line_number_ctermfg = "LightBlue",
 }
 
 return M
