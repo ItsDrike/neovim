1af1e3e5d6ec71c6c361d102ff69e2130b18ff0a
ItsDrike <itsdrike@protonmail.com>
2023-03-04 21:33:29 +0100

    Add mini.bufremove for proper buffer deleting

--- lua/svim/plugins/editor.lua
+++ lua/svim/plugins/editor.lua
@@ -83,6 +83,15 @@ return {
     },
   },
 
+  -- Remove buffers, preserving window layouts
+  {
+    "echasnovski/mini.bufremove",
+    keys = {
+      { "<leader>bd", function() require("mini.bufremove").delete(0, false) end, desc = "Delete Buffer" },
+      { "<leader>bD", function() require("mini.bufremove").delete(0, true) end, desc = "Delete Buffer (Force)" },
+    },
+  },
+
   -- Search/Replace in multiple files
   {
     "windwp/nvim-spectre",
