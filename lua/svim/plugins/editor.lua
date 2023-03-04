5cc538cbf17c96ed41b41e15ebbb3255cc17a7f0
ItsDrike <itsdrike@protonmail.com>
2023-03-04 21:32:16 +0100

    Add vim-illuminate plugin

--- lua/svim/plugins/editor.lua
+++ lua/svim/plugins/editor.lua
@@ -47,4 +47,39 @@ return {
       },
     },
   },
+
+  -- Highlight references of token under cursor
+  {
+    "RRethy/vim-illuminate",
+    event = { "BufReadPost", "BufNewFile" },
+    opts = {
+      -- providers: provider used to get references in the buffer, ordered by priority
+      providers = {
+          'lsp',
+          'treesitter',
+          'regex',
+      },
+      delay = 200
+    },
+    config = function(_, opts)
+      require("illuminate").configure(opts)
+
+      -- Set the keybinds after loading ftplugins, since a lot overwrite [[ and ]]
+      vim.api.nvim_create_autocmd("FileType", {
+        callback = function()
+          local buffer = vim.api.nvim_get_current_buf()
+          vim.keymap.set("n", "]]", function()
+            require("illuminate")["goto_next_reference"](false)
+          end, { desc = "Next Reference", buffer = buffer })
+          vim.keymap.set("n", "[[", function()
+            require("illuminate")["goto_prev_reference"](false)
+          end, { desc =  "Prev Reference", buffer = buffer })
+        end,
+      })
+    end,
+    keys = {
+      { "]]", function() require("illuminate")["goto_next_reference"](false) end, desc = "Next Reference" },
+      { "[[", function() require("illuminate")["goto_prev_reference"](false) end, desc = "Prev Reference" },
+    },
+  },
 }
