edd7151a7728e685f770d69dc322848836211d9f
ItsDrike <itsdrike@protonmail.com>
2023-03-06 22:06:49 +0100

    Add alpha dashboard plugin

---
+++ lua/svim/vars/dashboard.lua
@@ -0,0 +1,40 @@
+return {
+  logo = {
+    "░▒█▀▀▀█░▀█▀░█▀▀░█░░█░░█▀▀▄░█▀▀▄░▒█▄░▒█░▄░░░▄░░▀░░█▀▄▀█",
+    "░░▀▀▀▄▄░░█░░█▀▀░█░░█░░█▄▄█░█▄▄▀░▒█▒█▒█░░█▄█░░░█▀░█░▀░█",
+    "░▒█▄▄▄█░░▀░░▀▀▀░▀▀░▀▀░▀░░▀░▀░▀▀░▒█░░▀█░░░▀░░░▀▀▀░▀░░▒▀",
+  },
+  -- Amount of columns (characters) the logo takes up
+  -- (This is hard-coded, since logo string is unicode, and lua counts bytes, not chars with string.len)
+  logo_width = 54,
+  -- splash_text can be a string, list of strings (multiple lines), or a function returning either
+  ---@type function|string|table<string>
+  splash_text = function()
+    local possibilities = {
+      -- {}, -- no splash is an option too, for the clean look
+      {
+        "Nothing to see here",
+        "Move on ...",
+      }, -- multiline splash
+      "Denser than Emacs!",
+      "Hello there",
+      "90% bug free!",
+      "Cooler than Spock!",
+      "It's free software!",
+      "Free as in freedom",
+      "sqrt(-1) love you!",
+      "Vim, the final frontier",
+      "You're going too fast!",
+      "Licensed under GPL v3: Glorious People's License",
+      "Rule #1: it's never my fault",
+    }
+    math.randomseed(os.time())
+    return possibilities[math.random(#possibilities)]
+  end,
+  highlights = {
+    header = { fg = "blue" },
+    footer = { fg = "brown" },
+    buttons = { fg = "lightgray" },
+    shortcut = { fg = "yellow" },
+  }
+}
--- lua/svim/vars/highlights.lua
+++ lua/svim/vars/highlights.lua
@@ -1,7 +1,15 @@
+local dashboard_highlights = require("svim.vars.dashboard").highlights
+
 return {
   -- Cursor line & Line number
   CursorLine = { bg = "#2b2b2b" },
   CursorLineNr = { fg = "#1f85de" },
+
+  -- Dashboard (Alpha) highlights
+  AlphaHeader = dashboard_highlights.header,
+  AlphaFooter = dashboard_highlights.footer,
+  AlphaButtons = dashboard_highlights.buttons,
+  AlphaShortcut = dashboard_highlights.shortcut,
 
   -- Autocompletion (nvim-cmp) kind highlights
   CmpItemKindCopilot = { fg = "#6CC644" },
