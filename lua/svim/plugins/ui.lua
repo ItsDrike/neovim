e05d1ba97610ea3b6a011e7c50a18dbc2260faf3
ItsDrike <itsdrike@protonmail.com>
2023-03-07 02:31:54 +0100

    Add nvim-notify plugin

--- lua/svim/plugins/ui.lua
+++ lua/svim/plugins/ui.lua
@@ -55,6 +55,40 @@ return {
     },
   },
 
+  -- Better `vim.notify()`
+  {
+    "rcarriga/nvim-notify",
+    keys = {
+      {
+        "<leader>un",
+        function()
+          require("notify").dismiss({ silent = true, pending = true })
+        end,
+        desc = "Delete all Notifications",
+      },
+    },
+    opts = {
+      timeout = 3000,
+      max_height = function()
+        return math.floor(vim.o.lines * 0.75)
+      end,
+      max_width = function()
+        return math.floor(vim.o.columns * 0.75)
+      end,
+    },
+    init = function()
+      -- If noice is installed, we don't need to override vim.notify,
+      -- it does it for us already, and utilizes nvim-notify when needed.
+      -- Otherwise, override vim.notify manually on VeryLazy event, requiring the plugin, hence also loading it
+      local Util = require("svim.utils.plugins")
+      if not Util.has("noice.nvim") then
+        Util.on_very_lazy(function()
+          vim.notify = require("notify")
+        end)
+      end
+    end,
+  },
+
   -- dashboard (on vim enter without file/dir param)
   {
     "goolord/alpha-nvim",
