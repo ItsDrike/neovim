ec7cf92b751b6c54dcca71c94a9aa90c10c623c2
ItsDrike <itsdrike@protonmail.com>
2023-03-07 02:48:41 +0100

    Add bufferline plugin

---
+++ lua/svim/plugins/lines.lua
@@ -0,0 +1,32 @@
+local Icons = require("svim.vars.icons")
+
+return {
+  -- bufferline (show opened buffers on top)
+  {
+    "akinsho/bufferline.nvim",
+    event = "VeryLazy",
+    keys = {
+      { "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", desc = "Toggle pin" },
+      { "<leader>bP", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "Delete non-pinned buffers" },
+    },
+    opts = {
+      options = {
+        diagnostics = "nvim_lsp",
+        always_show_bufferline = true,
+        diagnostics_indicator = function(_, _, diag)
+          local ret = (diag.error and Icons.diagnostics.Error .. diag.error .. " " or "")
+            .. (diag.warning and Icons.diagnostics.Warning .. diag.warning or "")
+          return vim.trim(ret)
+        end,
+        offsets = {
+          {
+            filetype = "neo-tree",
+            text = "Neo-tree",
+            highlight = "Directory",
+            text_align = "left",
+          },
+        },
+      },
+    },
+  },
+}
