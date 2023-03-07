884b8cb0d0642f962eba64dc9917b029277a75d0
ItsDrike <itsdrike@protonmail.com>
2023-03-07 02:40:28 +0100

    Add dressing.nvim plugin, improving all nvim UIs

--- lua/svim/plugins/ui.lua
+++ lua/svim/plugins/ui.lua
@@ -89,6 +89,25 @@ return {
     end,
   },
 
+  -- Improve all UI interfaces (overrides vim.ui)
+    {
+    "stevearc/dressing.nvim",
+    lazy = true,
+    init = function()
+      -- Will be required on first UI vim.ui.input/vim.ui.select function call
+      ---@diagnostic disable-next-line: duplicate-set-field
+      vim.ui.select = function(...)
+        require("lazy").load({ plugins = { "dressing.nvim" } })
+        return vim.ui.select(...)
+      end
+      ---@diagnostic disable-next-line: duplicate-set-field
+      vim.ui.input = function(...)
+        require("lazy").load({ plugins = { "dressing.nvim" } })
+        return vim.ui.input(...)
+      end
+    end,
+  },
+
   -- dashboard (on vim enter without file/dir param)
   {
     "goolord/alpha-nvim",
