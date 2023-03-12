bea73e594d8bc859105648f5d18fafb4de2bd8e2
ItsDrike <itsdrike@protonmail.com>
2023-03-12 18:57:57 +0100

    Fix loading friendly-snippets to luasnip

--- lua/svim/plugins/autocomplete/init.lua
+++ lua/svim/plugins/autocomplete/init.lua
@@ -2,26 +2,27 @@ return {
   -- Snippets
   {
     "L3MON4D3/LuaSnip",
+    version = "*",
     event = "InsertEnter",
     dependencies = { "friendly-snippets" },
     opts = {
       history = true,
       delete_check_events = "TextChanged",
     },
-    config = function()
+    config = function(_, opts)
       local Path = require("svim.utils.path")
       local paths = {}
-      if require("svim.utils.plugins").has("friendly-snippets") then
-        paths[#paths + 1] = Path.join_paths(Path.plugins_dir, "friendly-snippets")
-      end
       local user_snippets = Path.join_paths(Path.config_dir, "snippets")
       if Path.is_directory(user_snippets) then
         paths[#paths + 1] = user_snippets
       end
 
       require("luasnip.loaders.from_lua").lazy_load()
-      require("luasnip.loaders.from_vscode").lazy_load({ paths = paths })
       require("luasnip.loaders.from_snipmate").lazy_load()
+      require("luasnip.loaders.from_vscode").lazy_load()
+      require("luasnip.loaders.from_vscode").load({ paths = paths })
+
+      require("luasnip").setup(opts)
     end
   },
 
