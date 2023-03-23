82e02f68b046efc7e35d1881e712de2bdf7b062f
ItsDrike <itsdrike@protonmail.com>
2023-03-23 14:42:01 +0100

    Add autocmd for highlights on yanking

--- lua/svim/config/autocmds.lua
+++ lua/svim/config/autocmds.lua
@@ -1,5 +1,17 @@
 -- This file is automatically loaded by lazyvim.plugins.config
 
+local Autocmds = require("svim.utils.autocmds")
+local augroup = Autocmds.augroup
+
+-- Highlight on yank
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = augroup("highlight_yank"),
+  callback = function()
+    vim.highlight.on_yank( { higroup = "Search", timeout = 100 } )
+  end,
+  desc = "Highlight text on yank",
+})
+
 -- Add some extra highlight groups
 for _, highlight_conf in ipairs(require("svim.vars.highlights")) do
   require("svim.utils.colorscheme").define_highlights(
