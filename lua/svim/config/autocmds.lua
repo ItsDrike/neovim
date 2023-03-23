83ffa7ff1196c07d009ad80d1399a981a9ecbe02
ItsDrike <itsdrike@protonmail.com>
2023-03-23 14:43:25 +0100

    Go to last location when re-opening a file

--- lua/svim/config/autocmds.lua
+++ lua/svim/config/autocmds.lua
@@ -49,3 +49,15 @@ for _, highlight_conf in ipairs(require("svim.vars.highlights")) do
     false
   )
 end
+
+-- Go to last loc when opening a buffer
+vim.api.nvim_create_autocmd("BufReadPost", {
+  group = augroup("last_loc"),
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local lcount = vim.api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+})
