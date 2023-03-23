6f9948ce63e4d544f0c0632fa5205d6d030055ac
ItsDrike <itsdrike@protonmail.com>
2023-03-23 14:47:33 +0100

    Start wakatime on cmd or BufReadPost/BufNewFile

--- lua/svim/plugins/extra/wakatime.lua
+++ lua/svim/plugins/extra/wakatime.lua
@@ -1,4 +1,17 @@
 return {
   -- Code time & habit tracking
-  "wakatime/vim-wakatime"
+  "wakatime/vim-wakatime",
+  event = { "BufReadPost", "BufNewFile" },
+  cmd = {
+    "WakaTimeApiKey",
+    "WakaTimeDebugEnable",
+    "WakaTimeDebugDisable",
+    "WakaTimeScreenRedrawEnable",
+    "WakaTimeScreenRedrawEnableAuto",
+    "WakaTimeScreenRedrawDisable",
+    "WakaTimeToday",
+    "WakaTimeCliLocation",
+    "WakaTimeCliVersion",
+    "WakaTimeFileExpert",
+  },
 }
