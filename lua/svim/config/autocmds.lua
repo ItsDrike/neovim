a8a87cffea51758dc7ebd17e1386a4b6017c87a2
ItsDrike <itsdrike@protonmail.com>
2023-03-23 14:42:28 +0100

    Make some filetypes closable with q

--- lua/svim/config/autocmds.lua
+++ lua/svim/config/autocmds.lua
@@ -12,6 +12,33 @@ vim.api.nvim_create_autocmd("TextYankPost", {
   desc = "Highlight text on yank",
 })
 
+-- close some filetypes with <q>
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup("close_with_q"),
+  pattern = {
+    "qf",
+    "help",
+    "man",
+    "notify",
+    "floatterm",
+    "lspinfo",
+    "lir",
+    "lsp-installer",
+    "null-ls-info",
+    "tsplayground",
+    "spectre_panel",
+    "startuptime",
+    "DressingSelect",
+    "Jaq",
+    "PlenaryTestPopup",
+  },
+  callback = function(event)
+    vim.bo[event.buf].buflisted = false
+    vim.keymap.set("n", "q", "<cmd>Close<CR>", { buffer = event.buf, silent = true })
+  end,
+  desc = "Close certain filetypes with q",
+})
+
 -- Add some extra highlight groups
 for _, highlight_conf in ipairs(require("svim.vars.highlights")) do
   require("svim.utils.colorscheme").define_highlights(
